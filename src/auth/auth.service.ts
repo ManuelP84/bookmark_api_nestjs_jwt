12ffb754f5d8e27e307f1eb0426fbb8a0e2fbbc4
Manuel Pineda Mendez <manuel.pineda@sofka.com.co>
2022-08-20 11:19:00 -0500

    JWT implementation with passport strategy

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,12 +2,17 @@ import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as argon from 'argon2';
-import { User } from '@prisma/client';
 import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import { JwtService } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AuthService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
   async signup(authDto: AuthDto) {
     try {
@@ -55,7 +60,24 @@ export class AuthService {
       throw new ForbiddenException('Credentials incorrect');
     }
     //Send back the user
-    delete user.hash;
-    return user;
+    // delete user.hash;
+    return this.signinToken(user.id, user.email);
+  }
+
+  async signinToken(userId: number, email: string): Promise<{ access_token:string }> {
+    const payload = {
+      sub: userId,
+      email,
+    };
+    const secret = this.config.get('JWT_SECRET_KEY');
+
+    const token =  await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret,
+    });
+
+    return {
+      access_token: token,
+    };
   }
 }
